3f8d3f467908811e078f178d2fc53f7e2c75473b
agmr999 <42304457+agmr999@users.noreply.github.com>
2018-09-02 11:07:36 -1200

    Add files via upload

---
+++ listaestructuradaligaduradoble.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo nodo;
+
+struct nodo{
+	int dato;
+	struct nodo *siguiente;
+	struct nodo *anterior;
+};
+
+nodo *inicio=NULL;
+nodo *ultimo=NULL;
+
+nodo *crearnodo(int dato){
+	nodo *nuevo=NULL;
+	nuevo=(nodo *)malloc(sizeof(nodo));
+	if(nuevo!=NULL){
+		nuevo->dato=dato;
+		nuevo->siguiente=NULL;
+		nuevo->anterior=NULL;
+	}
+	return nuevo;
+}
+
+void insertaini(int dato){
+	nodo *nuevo=NULL;
+	nuevo= crearnodo(dato);
+	if(inicio == NULL){
+		inicio=nuevo;
+		ultimo=nuevo;
+	}
+	else{
+		ultimo->siguiente=nuevo;
+		nuevo->anterior=ultimo;
+		ultimo=nuevo;
+	}
+}
+
+void imprimirFI(){
+	nodo *aux=ultimo;
+	while(aux!=NULL){
+		printf("%d\n",aux->dato);
+		aux=aux->anterior;
+	}
+}
+
+void imprimirIF(){
+	nodo *aux=inicio;
+	while(aux!=NULL){
+		printf("%d\n",aux->dato);
+		aux=aux->siguiente;
+	}
+	imprimirFI();
+}
+
+
+int main(int argc, char *argv[]) {
+	insertaini(2);
+	insertaini(4);
+	insertaini(5);
+	imprimirIF();
+	return 0;
+}
+
